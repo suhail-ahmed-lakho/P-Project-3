60083e451c943b1f4b53228d26ada044a8c0f91d
Suhail Ahmed <suhaillakho3@gmail.com>
2025-02-12 22:56:37 +0500

    suhail

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -2,8 +2,7 @@
 
 import * as React from 'react';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
-import { DayPicker } from 'react-day-picker';
-
+import { DayPicker, type CustomComponents } from 'react-day-picker';
 import { cn } from '@/lib/utils';
 import { buttonVariants } from '@/components/ui/button';
 
@@ -52,10 +51,6 @@ function Calendar({
           'aria-selected:bg-accent aria-selected:text-accent-foreground',
         day_hidden: 'invisible',
         ...classNames,
-      }}
-      components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
